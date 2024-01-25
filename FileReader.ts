55f30e8b39b091609ca1249b9ebf123c5c024d3a
j_lin <j_lin@zisouz.co.jp>
2024-01-25 14:33:54 +0900

    feat:Implement file reader and rule reader

--- FileReader.ts
+++ FileReader.ts
@@ -1,5 +1,57 @@
-class FileReader {
+import * as fs from 'fs';
+import * as readline from 'readline';
+import { requiredNames, requiredNamesIndices, names, JPNames } from './consts/required_names';
 
+class CsvLineReader {
+  private filePath: string;
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+
+  async readLines(): Promise<void> {
+    const fileStream = fs.createReadStream(this.filePath);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+
+    // Process each line
+    for await (const line of rl) {
+      const items = line.split(',');
+
+      // this.consoleProduct(items);
+
+      const product: Record<string, string> = {};
+      for (let [index, name] of requiredNames.entries()) {
+        // console.log(`====== ${name}: ${items[requiredNamesIndices[index]]}`);
+        product[name] = items[requiredNamesIndices[index]];
+      }
+
+      console.log('====== product: ');
+      console.table(product);
+    }
+  }
+
+  consoleProduct(items: string[]): void {
+    const DeomoTable: string[][] = [];
+    DeomoTable[0] = JPNames;
+    DeomoTable[1] = items;
+    const tableWidth = DeomoTable[0].length;
+    for (let i = 0; i < tableWidth; i++) {
+      if (i % 10 === 0) {
+        const chunkData = DeomoTable.map(row => row.slice(i, i + 10));
+        console.table(chunkData);
+      }
+    }
+  }
+
+
+  async processData(): Promise<void> {
+    await this.readLines();
+  }
 }
 
-export default FileReader;
+// Example usage
+const csvReader = new CsvLineReader('./21.csv');
+csvReader.processData();
---
+++ RuleReader.ts
@@ -0,0 +1,66 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+class CsvLineReader {
+  private filePath: string;
+  private names: string[] = [];
+  private JPNames: string[] = [];
+
+  private requiredNames: string[] = [];
+  private requiredNameIndices: Number[] = [];
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+
+  async readLines(): Promise<void> {
+    const fileStream = fs.createReadStream(this.filePath);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+
+    for await (const line of rl) {
+      const items = line.split(',');
+
+      // console.table(items);
+
+      // get all names
+      if ( !isNaN(parseInt(items[0])) ) {
+        this.names.push(items[1]);
+        this.JPNames.push(items[10]);
+        console.log('===== JP: ', items[10]);
+      }
+
+      if (line.includes('●')) {
+        this.requiredNames.push(items[1]);
+        console.log('====== index:', parseInt(items[0]));
+        this.requiredNameIndices.push(parseInt(items[0]) - 1);
+      }
+    }
+  }
+
+  writeToConsts(): void {
+    const outputFilePath = './consts/required_names.ts';
+    let dataToWrite = `export const requiredNames = ['${this.requiredNames.join("','")}'];`;
+
+    // all names
+    dataToWrite = dataToWrite + `\nexport const names = ['${this.names.join("','")}'];`;
+    dataToWrite = dataToWrite + `\nexport const JPNames = ['${this.JPNames.join("','")}'];`;
+
+
+    dataToWrite = dataToWrite + `\nexport const requiredNamesIndices = [${this.requiredNameIndices.join(",")}];`;
+
+
+    fs.writeFileSync(outputFilePath, dataToWrite);
+  }
+
+  async generateRequiredNames(): Promise<void> {
+    await this.readLines();
+    this.writeToConsts();
+  }
+}
+
+// Example usage
+const csvReader = new CsvLineReader('./testRule.csv');
+csvReader.generateRequiredNames();
